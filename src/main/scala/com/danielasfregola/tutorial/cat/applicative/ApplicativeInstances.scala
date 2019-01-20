52fc066fccb2aeb04c6ea5e4de8e77bd7c32e955
Dragan Glumac <dragan.glumac@gmail.com>
2019-01-20 18:10:19 +0000

    Applicative

--- src/main/scala/com/danielasfregola/tutorial/cat/applicative/ApplicativeInstances.scala
+++ src/main/scala/com/danielasfregola/tutorial/cat/applicative/ApplicativeInstances.scala
@@ -6,8 +6,27 @@ import com.danielasfregola.tutorial.cat._
 
 object ApplicativeInstances {
 
-  implicit val maybeApplicative: Applicative[Maybe] = ???
+  implicit val maybeApplicative: Applicative[Maybe] = new Applicative[Maybe] {
+    override def pure[A](a: A): Maybe[A] = Just(a)
 
-  implicit val zeroOrMoreApplicative: Applicative[ZeroOrMore] = ???
+    override def ap[A, B](boxF: Maybe[A => B])(boxA: Maybe[A]): Maybe[B] = (boxF, boxA) match {
+      case (Just(f), Just(a)) => Just(f(a))
+      case _ => Empty
+    }
+  }
+
+  implicit val zeroOrMoreApplicative: Applicative[ZeroOrMore] = new Applicative[ZeroOrMore] {
+    override def pure[A](a: A): ZeroOrMore[A] = OneOrMore(a, Zero)
+
+    override def ap[A, B](boxF: ZeroOrMore[A => B])(boxA: ZeroOrMore[A]): ZeroOrMore[B] = (boxF, boxA) match {
+        // There are at least 3 options
+        //   1 - run the first function for all As
+        //   2 - run all functions for all As
+        //   3 - create a zip function and then zip functions with As
+        // This solution implements option 1.
+      case (OneOrMore(f, _), OneOrMore(a, ta)) => OneOrMore(f(a), ap(boxF)(ta))
+      case _ => Zero
+    }
+  }
 
 }
